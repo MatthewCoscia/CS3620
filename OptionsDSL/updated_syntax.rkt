21bbb74a3dc478f63795e324b03406878f848399
DilanRocco <captaindilan12@gmail.com>
2025-03-15 10:55:32 -0400

    Add stock arguments to option strategy

--- OptionsDSL/updated_syntax.rkt
+++ OptionsDSL/updated_syntax.rkt
@@ -1,5 +1,3 @@
-
-
 #lang racket
 (require (for-syntax syntax/parse))
 
@@ -30,18 +28,26 @@
 (define-syntax (define-option-strategy stx)
   (syntax-parse stx
     [(_ strategy-name:id 
-        (action:action qty:positive-whole-qty type:option-type #:strike
-                       strike:positive-strike) ...)
+        #:ticker ticker:id
+        #:current-price current-price:expr
+        (action:action qty:positive-whole-qty
+                       type:option-type
+                       #:strike strike:positive-strike) ...)
      
+     #:with ticker-symbol (datum->syntax #'ticker (symbol->string (syntax->datum #'ticker)))
      #:with (action-sym ...) (map (λ (a) (datum->syntax #f (syntax->datum a))) 
                                   (syntax->list #'(action ...)))
      #:with (type-sym ...) (map (λ (t) (datum->syntax #f (syntax->datum t))) 
                                 (syntax->list #'(type ...)))
      #'(define strategy-name
-         (list (list 'action-sym qty 'type-sym strike) ...))]))
+         (hash 'ticker ticker-symbol
+               'current-price current-price
+               'legs (list (list 'action-sym qty 'type-sym strike) ...)))]))
 
 
 (define-option-strategy my-strat
+  #:ticker AAPL
+  #:current-price 100
   (buy 1 call #:strike 100)
   (sell 1 call #:strike 105)
   (buy 1 put #:strike 95))
